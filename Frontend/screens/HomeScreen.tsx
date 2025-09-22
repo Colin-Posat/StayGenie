7f71e93ee86a8ca2868589fe6652e455f2b851a2
Colin-Posat <colinposat@gmail.com>
2025-09-21 17:45:18 -0700

    url changed back

--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -328,8 +328,8 @@ interface Hotel {
   safetyJustification?: string; 
 }
 
-const BASE_URL='http://localhost:3003';
-//const BASE_URL="https://staygenie-wwpa.onrender.com"
+//const BASE_URL='http://localhost:3003';
+const BASE_URL="https://staygenie-wwpa.onrender.com"
 
 
 
